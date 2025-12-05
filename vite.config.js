86b3633086f11e8941514862eee78faa1ed8a5a3
ll <ll@lldeMac-mini.local>
2025-12-05 11:52:02 +0800

    modify last commit

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         display: "standalone",
         icons: [
           {
-            src: 'logo.png',
+            src: 'logo-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'logo.png',
+            src: 'logo-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
